af285ebcbf195387017de78d3d878f5c5afc1068
QuanChi2003 <109083423+QuanChi2003@users.noreply.github.com>
2025-10-01 14:56:53 +0700

    Update oauth2callback.js

--- api/oauth2callback.js
+++ api/oauth2callback.js
@@ -23,18 +23,7 @@ module.exports = async (req, res) => {
       res.statusCode = 400;
       return res.end('Missing code');
     }
-    const client = makeOAuthClient();
-    const { tokens } = await client.getToken(code);
-    client.setCredentials(tokens);
-
-    // Lấy thông tin người dùng
-    const oauth2 = google.oauth2({ version: 'v2', auth: client });
-    const { data } = await oauth2.userinfo.get();
-    tokens.user = {
-      email: data.email || null,
-      name: data.name || data.given_name || data.email || null,
-      picture: data.picture || null
-    };
+    const { tokens } = await makeOAuthClient().getToken(code);
 
     // Lưu token vào cookie (JSON + chữ ký HMAC)
     const payload = Buffer.from(JSON.stringify(tokens)).toString('base64url');
